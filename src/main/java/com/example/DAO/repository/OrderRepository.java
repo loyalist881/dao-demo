fc664ce374d591f87c2bdd9619d4d5b8c250ee37
Sokolov Pavel <sokolovpavel31@gmail.com>
2026-05-03 23:58:58 +0700

    Testing braching

--- src/main/java/com/example/DAO/repository/OrderRepository.java
+++ src/main/java/com/example/DAO/repository/OrderRepository.java
@@ -17,6 +17,13 @@ public class OrderRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final String script;
 
+
+
+
+
+
+
+
     public OrderRepository(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
         this.script = read("find_product_by_name.sql");
